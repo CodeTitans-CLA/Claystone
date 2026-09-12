0c54c122cf10ebf83e370dc8edafc77fe28341e0
imonrana <info@yahoo.com>
2026-09-12 16:15:01 +0600

    frist comit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import HeroSection from '@/components/Home/HeroSection';
+import Services from '@/components/Home/Services';
 import Image from 'next/image';
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <>
 
     <HeroSection></HeroSection>
+    <Services></Services>
      
     </>
   );
